27dbadc3a411a7e8890dfe67cde888fe23b143e8
Tanner Jackley <tannerjackley@gmail.com>
2025-03-25 10:12:03 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var x = req.query.x;
-var y = Math.atan2(x);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  acc = ratio * acc;
+  var x = req.query.x;
+  var y = Math.atan2(x);
   res.send(`Math.atan2() applied to ${x} is ${y}`);
 });
 
