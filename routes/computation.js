15c29ec820b7f2c936016c32aa80a804344670ac
Tanner Jackley <tannerjackley@gmail.com>
2025-03-25 10:18:27 -0500

    fixed bonus using atanh

--- routes/computation.js
+++ routes/computation.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var x = req.query.x;
-  var y = Math.atan2(x);
-  res.send(`Math.atan2() applied to ${x} is ${y}`);
+  var x = (Math.random() * 1.8) - 0.9;
+  var y = Math.atanh(x);
+  res.send(`Math.atanh() applied to ${x} is ${y}`);
 });
 
 module.exports = router;
